5515c5922fe55e633cb10fe79a1674c76ba37138
VHariVaraPrasad <harivennavalli9@gmail.com>
2024-08-21 17:29:26 +0530

    leads

---
+++ app/src/app/components/popup.js
@@ -0,0 +1,7 @@
+const Popup = () => {
+    <div className="flex gap-4 items-center justify-between p-4 md:p-5 border-b rounded-t">
+        <img alt="header image" class="w-10 h-9" src="https://crm.skill-capital.com/_next/static/media/employee_contact.2d215fd6.svg" style="color: transparent;"/>
+        <h3 class="text-xl font-semibold text-gray-900">Create Oppurtunity</h3>
+    
+    </div>
+}
