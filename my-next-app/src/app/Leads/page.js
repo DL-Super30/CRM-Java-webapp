4d1863e33804e6e482a4f63d3af4b68d09cf18d8
Nethala Nikhil <109343084+NethalaNikhil@users.noreply.github.com>
2024-08-23 21:54:11 +0530

    added in input.js

--- my-next-app/src/app/Leads/page.js
+++ my-next-app/src/app/Leads/page.js
@@ -81,7 +81,7 @@ const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
-  transform: 'translate(-50%, -100%)',
+  transform: 'translate(-50%, -70%)',
   width: 1100,
   bgcolor: 'background.paper',
   border: '2px solid #000',
--- my-next-app/src/components/input.js
+++ my-next-app/src/components/input.js
@@ -1,69 +1,505 @@
 "use client";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import { useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
 
-const Leadstatus = [
+const LeadStatus = [
   {
-    value: 'Select Lead Status'
+    value: "Select Lead Status",
+    label: "Select Lead Status",
   },
   {
-    value: 'Not Contacted'
+    value: "USD",
+    label: "Not Contacted",
   },
   {
-    value: 'Attempted'
+    value: "EUR",
+    label: "Attempted",
   },
   {
-    value: 'Warm Lead'
+    value: "BTC",
+    label: "Warm Lead",
   },
   {
-    value: 'Cold Lead'
-  }
+    value: "JPY",
+    label: "Cold Lead",
+  },
 ];
+
+const LeadSource = [
+  {
+    value: "Select Lead Source",
+    label: "Select Lead Source",
+  },
+  {
+    value: "None",
+    label: "None",
+  },
+  {
+    value: "Walk In",
+    label: "Walk In",
+  },
+  {
+    value: "Student Referal",
+    label: "Student Referal",
+  },
+  {
+    value: "Demo",
+    label: "Demo",
+  },
+  {
+    value: "Website",
+    label: "Website",
+  },
+  {
+    value: "USD",
+    label: "Website Chat",
+  },
+  {
+    value: "EUR",
+    label: "Inbound Call",
+  },
+  {
+    value: "BTC",
+    label: "Google AdWords",
+  },
+  {
+    value: "Facebook Ads",
+    label: "Facebook Ads",
+  },
+  {
+    value: "JPY",
+    label: "WhatsApp - Skill Capital",
+  },
+];
+
+const Stack = [
+  {
+    value: "Select Stack",
+    label: "Select Stack",
+  },
+  {
+    value: "BTC",
+    label: "Life Skills",
+  },
+  {
+    value: "Facebook Ads",
+    label: "Study Aboard",
+  },
+  {
+    value: "JPY",
+    label: "HR",
+  },
+];
+
+const ClassMode = [
+  {
+    value: "Select Class Mode",
+    label: "Select Class Mode",
+  },
+  {
+    value: "International Online",
+    label: "International Online",
+  },
+  {
+    value: "India Online",
+    label: "India Online",
+  },
+  {
+    value: "BLR ClassRoom",
+    label: "BLR ClassRoom",
+  },
+  {
+    value: "HYD ClassRoom",
+    label: "HYD ClassRoom",
+  },
+
+]
+
+const BatchTimings = [
+  {
+    value: "Select Class Mode",
+    label: "7AM-8AM",
+  },
+  {
+    value: "International Online",
+    label: "8AM-9AM",
+  },
+  {
+    value: "India Online",
+    label: "9AM-10AM",
+  },
+  {
+    value: "BLR ClassRoom",
+    label: "11AM-12AM",
+  },
+  {
+    value: "HYD ClassRoom",
+    label: "12PM-1PM",
+  },
+  {
+    value: "1PM-2PM",
+    label: "1PM-2PM",
+  },
+  {
+    value: "2PM-3PM",
+    label: "2PM-3PM",
+  },
+  {
+    value: "3PM-4PM",
+    label: "3PM-4PM",
+  },
+  {
+    value: "4PM-5PM",
+    label: "4PM-5PM",
+  },
+  {
+    value: "6PM-7PM",
+    label: "6PM-7PM",
+  },
+  {
+    value: "7PM-8PM",
+    label: "7PM-8PM",
+  },
+  {
+    value: "8PM-9PM",
+    label: "8PM-9PM",
+  },
+
+
+
+]
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "HR Business Partner",
+  "HR Generalist Core HR",
+  "HR Analytics",
+  "Spoken English",
+  "Public Speaking",
+  "Communication Skills",
+  "Soft Skills",
+  "Personality Development",
+  "Aptitude",
+  "IELTS",
+  "TOEFL",
+  "PTE",
+  "GRE",
+  "GMAT",
+  "Recruitment Specialist",
+  "Payroll Specialist",
+  "Learning and Development",
+  "Others",
+  "Finance",
+  "Competitive Exams",
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
 export default function BasicTextFields() {
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        "& > :not(style)": { m: 1, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
     >
-      <div className='flex justify-between'>
-        <div className=''>
+      <div className="flex justify-between">
+        <div className="">
           <label htmlFor="name">Name</label>
-          <TextField className='me-10' style={{width:'31rem'}} id="name"  variant="standard" placeholder='Name' />
+          <TextField
+            className="me-10"
+            style={{ width: "31rem" }}
+            id="name"
+            variant="standard"
+            placeholder="Name"
+          />
         </div>
-        <div className=''>
+        <div className="">
           <label htmlFor="leadStatus">Leads Status</label>
-          <TextField className='' style={{width:'31rem'}} id="leadStatus" variant="standard" placeholder='Leads Status' />
+          <TextField
+            select
+            defaultValue="Select Lead Status"
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Leads Status"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          >
+            {LeadStatus.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </div>
       </div>
 
-      <div className='flex justify-between'>
-        <div className=''>
+      <div className="flex justify-between">
+        <div className="">
           <label htmlFor="cc">CC</label>
-          <TextField className='me-10' style={{width:'31rem'}} id="name"  variant="standard" placeholder='Name' />
+          <TextField
+            className="me-10"
+            defaultValue="91"
+            style={{ width: "31rem" }}
+            id="name"
+            variant="standard"
+            placeholder="Name"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          />
         </div>
-        <div className=''>
-          <label htmlFor="leadSource">Leads Source</label>
-          <TextField className='' style={{width:'31rem'}} id="leadStatus" variant="standard" placeholder='Leads Status' />
+        <div className="">
+          <label htmlFor="leadsource">Leads Source</label>
+          <TextField
+            select
+            defaultValue="Select Lead Source"
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Leads Status"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          >
+            {LeadSource.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </div>
       </div>
 
-      <div className='flex justify-between'>
-        <div className=''>
+      <div className="flex justify-between">
+        <div className="">
           <label htmlFor="cc">Phone</label>
-          <TextField className='me-10' style={{width:'31rem'}} id="phone"  variant="standard" placeholder='Phone' />
+          <TextField
+            className="me-10"
+            style={{ width: "31rem" }}
+            id="phone"
+            variant="standard"
+            placeholder="Phone"
+          />
         </div>
-        <div className=''>
+        <div className="">
           <label htmlFor="stack">Stack</label>
-          <TextField className='' style={{width:'31rem'}} id="leadStatus" variant="standard" placeholder='Leads Status' />
+          <TextField
+            select
+            defaultValue="Select Stack"
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Leads Status"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          >
+            {Stack.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </div>
       </div>
 
+      <div className="flex justify-between">
+        <div className="">
+          <label htmlFor="email">Email</label>
+          <TextField
+            className="me-10"
+            style={{ width: "31rem" }}
+            id="phone"
+            variant="standard"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="">
+          <FormControl variant="standard" sx={{ m: 0, minWidth: 120 }}>
+            <InputLabel id="demo-multiple-chip-label">Courses</InputLabel>
+            <Select
+              className="w-[31.2rem]  border-b-4 border-black-500 "
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={personName}
+              onChange={handleChange}
+              input={
+                <OutlinedInput
+                  id="select-multiple-chip"
+                  sx={{
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      border: 'none',
+                    },
+                  }}
+                />
+              }
+              renderValue={(selected) => (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {names.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, personName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div className="">
+          <label htmlFor="feequoted">Fee Quoted</label>
+          <TextField
+            className="me-10"
+            style={{ width: "31rem" }}
+            id="name"
+            variant="standard"
+            placeholder="Fee Quoted"
+            type="number"
+            defaultValue={1}
+            InputProps={{
+              inputProps: {
+                min: 0, 
+                step: 1 
+              }
+            }}
+          />
+        </div>
+        <div className="">
+          <label htmlFor="leadStatus">Class Mode</label>
+          <TextField
+            select
+            defaultValue="Select Class Mode"
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Leads Status"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          >
+            {ClassMode.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <label htmlFor="batchtiming">Batch Timing</label>
+          <TextField
+            className="me-10"
+            select
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Batch Timing"
+            InputProps={{ style: { fontWeight: "bold" } }}
+            SelectProps={{
+              MenuProps: {
+                PaperProps: {
+                  style: {
+                    maxHeight: 200,
+                    overflowY: 'auto',
+                  },
+                },
+              },
+            }}
+          >
+            {BatchTimings.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+        <div className="">
+          <label htmlFor="nextfollowup">Next FollowUp</label>
+          <TextField
+            select
+            style={{ width: "31rem" }}
+            id="Next Followup"
+            variant="standard"
+            placeholder="Leads Status"
+            InputProps={{ style: { fontWeight: "bold" } }}
+          >
+          </TextField>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <label htmlFor="batchtiming">Batch Timing</label>
+          <TextField
+            className="me-10"
+            select
+            style={{ width: "31rem" }}
+            id="leadStatus"
+            variant="standard"
+            placeholder="Batch Timing"
+            InputProps={{ style: { fontWeight: "bold" } }}
+            SelectProps={{
+              MenuProps: {
+                PaperProps: {
+                  style: {
+                    maxHeight: 200,
+                    overflowY: 'auto',
+                  },
+                },
+              },
+            }}
+          >
+            {BatchTimings.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+      </div>
     </Box>
   );
 }
